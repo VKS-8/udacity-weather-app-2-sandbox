e4db850055a23f14594b051da627a0c7a7eeeb58
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-07-19 19:07:17 -0500

    Install dependencies Try to set up the GET / POST Try to clean the code Really confused at this point

--- server.js
+++ server.js
@@ -2,17 +2,22 @@
 projectData = {};
 
 // Set port
-const port = 5500;
+const port = process.env.PORT || 5501;
+
+require('dotenv').config();
+
+// To make requests
+const axios = require('axios');
 
 // Require Express to run server and routes
 const express = require('express');
-app.use(express());
+// app.use(express());
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+// Parse responses
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -21,13 +26,48 @@ app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
 
-// Initialize the main project folder
+// Initialize the app
 app.use(express.static('website'));
 
-// Test server
+// Returns weather data as an object
 app.get('/', (req, res) => {
-  res.send("Your server is working");
+  let zip = document.querySelector('#zip').value;
+  let countryCode = document.querySelector('#countryCode').value;
+  countryCode.toUpperCase();
+  let units = document.querySelector('#units').value;
+  const options = {
+    method: 'GET',
+    url: `http://openweathermap/data/2.5/weather?zip=${zip},${countryCode}&units=${units}&appid=${process.env.API_KEY}`,
+    headers: {
+    //   'X-OpenWeatherMap-Key': process.env.API_Key,
+    //   'X-OpenWeatherMap-Host': 'api.openweathermap.org'
+      'Content-Type': 'application/json'
+    }
+  }
+
+  axios.request(options)
+    .then(function(response) {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 });
+
+app.post('/', getWeatherData);
+
+function getWeatherData(req, res) {
+  projectData.push(req.body);
+}
+
+// app.post('/data', (req, res) => {
+//   projectData = req.body;
+//   projectData.temperature = newData.tempature;
+//   projectData.date = newData.date;
+//   projectData.userResponse = newData.userResponse;
+//   res.send(projectData);
+// })
 
 
 // Setup Server
--- website/app.js
+++ website/app.js
@@ -1,15 +1,15 @@
 /* Global Variables */
-let zip = document.querySelector('#zip');
-let countryCode = document.querySelector('#countryCode');
-countryCode.value.toUpperCase();
-let feelings = document.querySelector('#feelings');
-let units = document.querySelector('#units');
+// let zip = document.querySelector('#zip');
+// let countryCode = document.querySelector('#countryCode');
+// countryCode.value.toUpperCase();
+// let feelings = document.querySelector('#feelings');
+// let units = document.querySelector('#units');
 let notificationPopUp = document.querySelector('#notificationPopUp');
-let content = document.querySelector('#content');
-let date = document.querySelector('#date');
-let temp = document.querySelector('#temp');
+// let content = document.querySelector('#content');
+// let date = document.querySelector('#date');
 const submitbtn = document.querySelector('#generate');
-
+// const apiKey = '0cbd903252e3f29c6f6ad83595b63ba3';
+// const baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
 
 // Create a new date instance dynamically with JS
 let currentDate = new Date();
@@ -18,22 +18,12 @@ let newDate = currentDate.getMonth()+'.'+ currentDate.getDate()+'.'+ currentDate
 // App data
 const currentWeather = {};
 
-// currentWeather = {
-//   units: "celsius"
-// }
-
-// App base tempurature scale
-const Kelvin = 273;
-
-// Api key (not secured)
-const apiKey = '0cbd903252e3f29c6f6ad83595b63ba3';
-
 // Check for geolocation of user
 function geolocationCheck () {
   if(!'geolocation' in navigator) {
     notificationPopUp.style.display = 'block';
     notificationPopUp.innerHTML = '<p>Sorry, it looks like your device does not support geolocation. <br> Please enter your zip code and country code</p>';
-    return;
+    getWeatherDataZip(zip, countryCode, units);
   } else {
     navigator.geolocation.getCurrentPosition(setPosition, showError);
   }
@@ -43,7 +33,7 @@ function geolocationCheck () {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
 
-    getWeatherData(latitude, longitude);
+    getWeatherDataLL(latitude, longitude);
   }
 
   // Error handling for geolocation
@@ -51,36 +41,78 @@ function geolocationCheck () {
     notificationPopUp.style.display = "block";
     notificationPopUp.innerHTML = `<p> ${error.message} </p>`;  // Note: should not display standard app error messages as they can display sensitive information
   }
-
-  // Retrieve data from API provider using geolocation data
-  function getWeatherData(latitude, longitude) {
-    let apiLatLon = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-
-    // Check for data retrieval
-    console.log(apiLatLon);
-
-    fetch(apiLatLon)
-      .then(function(response){
-        let data = response.json();
-        return data;
-      })
-      .then(function(data) {
-        currentWeather.temperature.value = Math.floor(data.main.temp - kelvin);
-        currentWeather.description = data.weather[0].description;
-        currentWeather.city = data.name;
-        currentWeather.country = data.sys.country;
-      })
-      .then(function() {
-        updateUI();
-    });
-  }
 }
+
+// Retrieve data from API provider using geolocation data
+function getWeatherDataLL(latitude, longitude) {
+  // let apiLatLon = `baseUrl?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+  // Check for data retrieval
+  // console.log(apiLatLon);
+
+  fetch(apiLatLon)
+    .then(function(response){
+      let data = response.json();
+      return data;
+    })
+    .then(function(data) {
+      currentWeather.temperature.value = Math.floor(data.main.temp);
+      currentWeather.description = data.weather[0].description;
+      currentWeather.city = data.name;
+      currentWeather.country = data.sys.country;
+    })
+    .then(function() {
+      updateUI();
+  });
+}
+
+const getWeatherDataZip = async (zip, countryCode, units) => {
+  // let zip = document.querySelector('#zip');
+  // let countryCode = document.querySelector('#countryCode');
+  // countryCode.value.toUpperCase();
+  // let units = document.querySelector('#units');
+  let apiByZipCode = `baseUrl?zip=${zip},${countryCode}&units=${units}&appid=${apiKey}`;
+  const response = await fetch(apiByZipCode);
+  try {
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log('Error:', error);
+  }
+};
+
 // Update the user's UI with current weather information
 function udpateUI() {
+  let feelings = document.querySelector('#feelings');
+  let content = document.querySelector('#content');
+  let date = document.querySelector('#date');
   date.innerHTML = newDate;
   temp.innerHTML = `${currentWeather.temperature.value}◘<span>C</span>`;
-  content.innerHTML = `The weather in ${currentWeather.city}, ${currentWeather.country} is ${currentWeather.description}.`
+  content.innerHTML = `The weather in ${currentWeather.city}, ${currentWeather.country} is ${currentWeather.description} and you are feeling ${feelings}.`
 }
 
 
-// submitbtn.addEventListener('click', getWeatherData());
+document.getElementById('generate').addEventListener('click', async () => {
+  // if geolocationCheck ()
+
+  const postData = async (url = '', data = {}) => {
+    const response = await fetch('http://localhost:5501/', {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    try {
+      const newData = await response.json();
+      return newData;
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  postData('/', {zip: 74401, countryCode: US, units: imperial});
+
+});
